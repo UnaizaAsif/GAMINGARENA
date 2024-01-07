2d54b118d3eaecf5de807ebfb857b24454ab4bcf
waniamasood <waaniamasood@gmail.com>
2024-01-07 21:04:01 +0500

    added ROCKPAPERSCISSORS

--- ROCKPAPERSCISSORS.c
+++ ROCKPAPERSCISSORS.c
@@ -4,13 +4,13 @@ int main(){
 	//0==Rock, 1==Paper, 2==Scissors
 	printf("\n \t\tROCK PAPER SCISSORS GAME! \n");
 	printf("lets start! \n");
-	int usermove, compmove;
+	int usermove, compmove,score=0,compscore=0;
 	while(1){
 		printf("\nEnter your move: \t (0) for Rock \t (1) for Paper \t (2) for Scissor \t (3) to exit\n");
 		scanf("%d", &usermove);
 		compmove= rand()%3;      //0,1,2
 		if (usermove==3){
-            printf("Existing the game..\n");
+            printf("Exiting the game..\n");
             break;
 		}
         else if(usermove<0 || usermove>2){
@@ -20,30 +20,47 @@ int main(){
 		else if(usermove==0 && compmove==1){
 			printf("Computer chose %d", compmove);
 			printf("\nYou lost! \t Paper beats Rock");
+			compscore++;
 		}
 		else if(usermove==0 && compmove==2){
 			printf("Computer chose %d", compmove);
 			printf("\nYou won! \t Rock beats Scissor");
+			score++;
 		}
 		else if(usermove==1 && compmove==0){
 			printf("Computer chose %d", compmove);
 			printf("\nYou won! \t Paper beats Rock");
+			score++;
 		}
 		else if(usermove==1 && compmove==2){
 			printf("Computer chose %d", compmove);
 			printf("\nYou lost! \t Scissor beats Paper");
+			compscore++;
 		}
 		else if(usermove==2 && compmove==0){
 			printf("Computer chose %d", compmove);
 			printf("\nYou lost! \t Rock beats Scissor");
+			compscore++;
 		}
 		else if(usermove==2 && compmove==1){
 			printf("Computer chose %d", compmove);
 			printf("\nYou won! \t Scissor beats Paper");
+			score++;
 		}
 		else{
 			printf("Computer chose %d", compmove);
 			printf("\nIts a draw!");
 		}
 	}
+	printf("\n Your score is: %d \n",score);
+    printf(" Computer score is: %d \n",compscore);
+	if(compscore>score){
+		printf("Computer won! \n");
+	}else if(compscore<score){
+		printf("You Won! \n");
+	}else{
+		printf("Its A Draw! \n");
+	}
+	return 0;
+
 }
